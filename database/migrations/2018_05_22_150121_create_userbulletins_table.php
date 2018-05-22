066c9ef214ef972541a596d5c0688efff5e3f15a
victoralagwu <victoralagwu@gmail.com>
2018-05-22 16:07:16 +0100

    First Commit

---
+++ database/migrations/2018_05_22_150121_create_userbulletins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserbulletinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userbulletins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('bulletin_id');
+            $table->integer('subscribe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('userbulletins');
+    }
+}
